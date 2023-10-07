560385f6af72d87bfa4249610ff9ea7c2c2710dd
yang <2638274013@qq.com>
2023-10-07 16:55:01 +0800

    新增乾坤微前端配置项

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,62 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import pinia from './store/index.js'
-
 import './router/routeGuard'
+import mitt from 'mitt'
+import {
+  renderWithQiankun,
+  qiankunWindow
+} from 'vite-plugin-qiankun/dist/helper'
+import { registerMicroApps, start } from 'qiankun'
+import qiankunConfig from '@/configure/qiankunConfig'
 
-createApp(App).use(router).use(pinia).mount('#app')
+const app = createApp(App)
+
+// 子应用挂载函数
+function render(props: any) {
+  const { container } = props
+  app.use(router)
+  app.mount(
+    container ? container.querySelector('#indexContainer') : '#indexContainer'
+  ) // 这个ID为index.html中div的ID名称
+  app.config.globalProperties.mittBus = mitt()
+}
+
+// 当做为子应用时
+if (qiankunConfig.qiankunType == 'Sub') {
+  renderWithQiankun({
+    mount(props) {
+      render(props)
+      // store.commit("updateUserInfo",props);
+      // 在vue全局注册主应用的状态监听和状态改变事件
+      app.config.globalProperties.$onGlobalStateChange =
+        props.onGlobalStateChange
+      app.config.globalProperties.$setGlobalState = props.setGlobalState
+    },
+    bootstrap() {
+      console.log('bootstrap')
+    },
+    unmount() {
+      console.log('unmount')
+      app.unmount()
+    },
+    update() {
+      console.log('update')
+    }
+  })
+
+  if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+    render({})
+  }
+}
+
+// 当做为主应用时
+if (qiankunConfig.qiankunType == 'main') {
+  // 注册子应用
+  registerMicroApps(qiankunConfig.registerMicroApps)
+
+  // 启动项目
+  start()
+}
+
+app.use(router).use(pinia).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,10 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // 兼容低版本浏览器
 import legacy from '@vitejs/plugin-legacy'
 
+// 配置乾坤微前端
+import qiankun from 'vite-plugin-qiankun'
+const useDevMode = true // 如果是在主应用中加载子应用vite,必须打开这个,否则vite加载不成功, 单独运行没影响
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -36,7 +40,8 @@ export default defineConfig({
     }),
     legacy({
       targets: ['defaults', 'not IE 11']
-    })
+    }),
+    qiankun('appSub1', { useDevMode }) // 当作为子应用事才需要，子应用名为主应用注册的名字
   ],
 
   resolve: {
