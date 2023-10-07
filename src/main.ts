7947bf7820a59e512045355cf7923fe39eab24b4
yang <2638274013@qq.com>
2023-10-07 14:05:47 +0800

    去除冗余代码

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import pinia from './store/index.js'
-import 'lib-flexible'
-import Vconsole from 'vconsole'
 import { Toast } from 'vant'
 import 'vant/es/toast/style'
 import './router/routeGuard'
-
-import.meta.env.MODE == 'test' ? new Vconsole() : ''
 
 createApp(App).use(router).use(pinia).use(Toast).mount('#app')
