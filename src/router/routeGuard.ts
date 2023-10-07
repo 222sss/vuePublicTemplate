0fdbcef3fd4e4e1555aa9716b9fd6fb916f750f7
yang <2638274013@qq.com>
2023-10-07 13:54:45 +0800

    去除路由拦截和项目初始化

--- src/router/routeGuard.ts
+++ src/router/routeGuard.ts
@@ -1,18 +1,19 @@
 import router from './index'
 
 router.beforeEach(async (to, from, next) => {
-  if (to.path !== '/login') {
-    if (
-      !localStorage.getItem('token') ||
-      !JSON.parse(localStorage.getItem('token')).token ||
-      new Date().getTime() >
-        JSON.parse(localStorage.getItem('token')).createAt + 28800000
-    ) {
-      next({ path: '/login' })
-    } else {
-      next()
-    }
-  } else {
-    next()
-  }
+  // if (to.path !== '/login') {
+  //   if (
+  //     !localStorage.getItem('token') ||
+  //     !JSON.parse(localStorage.getItem('token')).token ||
+  //     new Date().getTime() >
+  //     JSON.parse(localStorage.getItem('token')).createAt + 28800000
+  //   ) {
+  //     next({ path: '/login' })
+  //   } else {
+  //     next()
+  //   }
+  // } else {
+  //   next()
+  // }
+  next()
 })
